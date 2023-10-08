a19bf3efe256557e1451c85473a7dbd4a45d8fbb
Bharath Kumar Chowdary Gundala <bharathkumarchowdary006@gmail.com>
2023-10-08 22:37:28 +0530

    Initial Commit

---
+++ Personal Finance Calculators/Personal_Finance_CalculatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Finance_CalculatorsApp.swift
+//  Personal Finance Calculators
+//
+//  Created by Bharath Kumar Gundala on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Finance_CalculatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
